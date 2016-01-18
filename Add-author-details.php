a7fbabe0b275aff9248c3b2e6409affb5188783f
Nicholas Duell <studio@duellingpixels.com>
2016-01-18 17:08:24 +1100

    Update Add-author-details.php

--- Add-author-details.php
+++ Add-author-details.php
@@ -1,6 +1,5 @@
 <?php
 
-	
 /** Replace the standard loop with our custom loop */
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop',  'sgr_author_info', 20);
@@ -28,23 +27,22 @@ add_action( 'genesis_loop',  'sgr_author_info', 20);
     $bio = $curauth->description;
     $name = $curauth->display_name;
     $website = $curauth->user_url; 
-    
+    $entry_author = get_avatar( get_the_author_meta( 'email' ), 200 );
        
 	echo apply_filters('the_content', get_post_meta($author_id->ID, $bio, true));
     
 	echo '<div class="entry">';
-		    echo '<p>All posts by : ' . $name . '</p>';
+			//$author_link  = get_author_posts_url( get_the_author_meta( 'ID' ) );
+			printf( '<div class="author-avatar">%s</div>', $entry_author );
 			echo '<p>About ' . $name . ' ' . $bio . '</p>';
 			echo '<p>Website: <a href="' . $website . '">' . $website . '</p>';
 	echo '</div>';
 	
 			
-	echo '<h3 class="entry-title" style="text-align: center"> Recent Blog Posts</h3> ';
+	echo '<h4 class="entry-title" style="text-align: center"> All posts by ' . $name . '</h4> ';
 			
-
 	genesis_standard_loop();
 }
-
 
 
 genesis();
